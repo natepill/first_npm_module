6ea2bf5c41736f5d080d52a2bd394ab2c52da26a
Nathan Pillai <natepill@gmail.com>
2019-02-23 23:48:47 -0800

    average function

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+exports.printMsg = function() {
+    console.log("Hello! This is Nathan's npm module. Use me!");
+}
+
+exports.average = function(arr) {
+    
+}
+
+// natepill_first_npm_module
